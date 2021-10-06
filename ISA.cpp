2ef8226c848f3a80d669f30ef3cddf41fa2156a8
gino <lorenzo.rondinelli48@gmail.com>
2021-10-06 17:57:48 +0200

    Update ISA.cpp

--- ISA.cpp
+++ ISA.cpp
@@ -1,7 +1,30 @@
 #include "ISA.h"
-#include <stdlib.h>
-#pragma warning(disable:4996)
+#pragma warning (disable:4996)
+void add_(cpu cpu_,char* sx, char* dx) {
 
-void move(R r,int s,int d) {
+	if (sx[0] != 'r' && dx[0] == 'r'){
+		
+		char s_ = sx[0];                                 
+		char d_ = dx[1];
+		cpu_->r[d_ - '0'].value += s_ - '0';
+
+	}
 	
+
+}
+void console_(cpu cpu_,char*packet) {
+
+	char *function = strtok(packet," ");
+	char* sx = strtok(NULL, " ");
+	char* dx = strtok(NULL, " ");
+
+		if (strcmp(function,add)==NULL) {
+			printf("gino");
+			printf("%s %s %s", function, sx, dx);
+			
+			add_(cpu_, sx, dx);
+
+
+	}
+		
 }
