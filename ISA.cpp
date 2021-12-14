3c9344676c564eaa309a4f59e5ab0eacae7bffc1
gino <lorenzo.rondinelli48@gmail.com>
2021-12-14 01:17:00 +0100

    Update ISA.cpp

--- ISA.cpp
+++ ISA.cpp
@@ -2,29 +2,69 @@
 #pragma warning (disable:4996)
 void add_(cpu cpu_,char* sx, char* dx) {
 
-	if (sx[0] != 'r' && dx[0] == 'r'){
-		
-		char s_ = sx[0];                                 
-		char d_ = dx[1];
+	char s_; 
+	char d_;
+
+
+	if (sx[0] != 'r' && dx[0] == 'r') {
+
+		s_ = sx[0];
+		d_ = dx[1];
 		cpu_->r[d_ - '0'].value += s_ - '0';
 
 	}
-	
+	else if (sx[0] == 'r' && dx[0] == 'r') {
 
-}
-void console_(cpu cpu_,char*packet) {
-
-	char *function = strtok(packet," ");
-	char* sx = strtok(NULL, " ");
-	char* dx = strtok(NULL, " ");
-
-		if (strcmp(function,add)==NULL) {
-			printf("gino");
-			printf("%s %s %s", function, sx, dx);
-			
-			add_(cpu_, sx, dx);
-
+		s_ = sx[1];
+		d_ = dx[1];
+		cpu_->r[d_ - '0'].value += cpu_->r[s_-'0'].value;
 
 	}
-		
+	else
+		printf("error!\n");
+}
+void sub_(cpu cpu_, char* sx, char* dx) {
+
+	char s_;
+	char d_;
+
+	if (sx[0] != 'r' && dx[0] == 'r') {
+
+		s_ = sx[0];
+		d_ = dx[1];
+		cpu_->r[d_ - '0'].value -= s_ - '0';
+
+	}
+	else if (sx[0] == 'r' && dx[0] == 'r') {
+
+		s_ = sx[1];
+		d_ = dx[1];
+		cpu_->r[d_ - '0'].value -= cpu_->r[s_-'0'].value;
+
+	}
+	else
+		printf("error!\n");
+}
+
+void mv_(cpu cpu_, char* sx, char* dx) {
+
+	char s_;
+	char d_;
+
+	if (sx[0] != 'r' && dx[0] == 'r') {
+
+		s_ = sx[0];
+		d_ = dx[1];
+		cpu_->r[d_ - '0'].value = s_-'0';
+
+	}
+	else if (sx[0] == 'r' && dx[0] == 'r') {
+
+		s_ = sx[1];
+		d_ = dx[1];
+		cpu_->r[d_ - '0'].value = cpu_->r[s_-'0'].value;
+
+	}
+	else
+		printf("error!");
 }
