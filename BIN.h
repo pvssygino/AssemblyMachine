06777226b57b9483fadf25f0bc5ee6f1a6af5a76
gino <lorenzo.rondinelli48@gmail.com>
2021-12-18 00:40:37 +0100

    Add files via upload

---
+++ BIN.h
@@ -0,0 +1,13 @@
+#ifndef BIN_H
+#define BIN_H
+
+//Gruppo Aritmetico-Logico
+
+#define LOGIC_GROUP "0100"
+
+#define ADD_CODE "0000"
+#define SUB_CODE "0001"
+#define NOT_CODE "0010"
+#define AND_CODE "0011"
+
+#endif
